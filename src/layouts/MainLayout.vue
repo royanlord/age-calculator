b250a199a9a4bc621c3d20075d83b257b172876a
royanlord <royanfarid32@gmail.com>
2024-09-08 22:31:02 +0700

    update button

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -40,10 +40,13 @@
         </div>
         <div class="w-full flex justify-center flex-col mt-6">
             <button 
-                class="mx-auto border-2 w-1/5 border-solid border-orange-500 hover:bg-orange-500 hover:text-white bg-white rounded-md px-4 py-1"
+                class="mx-auto border-2 w-1/5 border-solid hover:bg-orange-400 text-white bg-orange-500 rounded-md px-4 py-1 active:bg-orange-600 flex items-center justify-center"
                 @click="handleClick"
             >
-                {{ showResult ? 'Reset' : 'Check' }}
+                <span class="mr-1">{{ showResult ? 'Reset' : 'Check' }}</span>
+                <svg v-if="showResult" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="15" fill="white">
+                    <path d="M463.5 224l8.5 0c13.3 0 24-10.7 24-24l0-128c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8l119.5 0z"/>
+                </svg>
             </button>
         </div>
 
@@ -274,7 +277,7 @@
 
     const checkAge = () => {
         const birthDate = dayjs(`${year.value}-${month.value}-${day.value}`);
-        
+
         if (!(parseInt(day.value)) || !(parseInt(month.value)) || !(parseInt(year.value))) {
             alert('Please enter a valid date')
             return
